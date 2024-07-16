7a2dca9f1e2610e223ced7c1a40f8db9a09e8541
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-07-16 13:53:45 +0100

    added a website to server and test with, also added path property for navigating to the correct folder.

--- Models/WebsiteModel.cs
+++ Models/WebsiteModel.cs
@@ -4,5 +4,6 @@ public class WebsiteModel
 {
     public string Name { get; set; }
     public int Port { get; set; }
+    public string Path { get; set; }
     public string DefaultPage { get; set; }
 }
